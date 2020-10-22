5178a045d3413c537b2e71fdb138adec2cf23bc2
Niklas <n.taube@htlkrems.at>
2020-10-22 14:15:10 +0200

    mein Github-Passwort ist scheiße

--- aernautica_imperiali/Program.cs
+++ aernautica_imperiali/Program.cs
@@ -3,10 +3,6 @@
 namespace aernautica_imperiali {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.Write("test");
-            Console.Write("Jonas");
-            Console.WriteLine("Niklas");
         }
     }
 }

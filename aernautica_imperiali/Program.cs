ec1d755d262622bc6da5ac8047de2e1470bd9ad5
JonasNagi <j.nagelmaier@htlkrems.at>
2020-10-22 13:44:34 +0200

    test

---
+++ aernautica_imperiali/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace aernautica_imperiali {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.Write("test");
+        }
+    }
+}

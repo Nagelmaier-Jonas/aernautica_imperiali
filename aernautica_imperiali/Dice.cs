0ee70b2b20780634bf469550f669cdc0fe18f672
Niklas <n.taube@htlkrems.at>
2020-10-26 19:43:29 +0100

    Dice

---
+++ aernautica_imperiali/Dice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aernautica_imperiali
+{
+    public class Dice
+    {
+        private static Dice instance;
+
+        private Dice()
+        {
+            
+        }
+
+        public static Dice getInstance()
+        {
+            return instance;
+        }
+        
+        Random random = new Random();
+
+        public int Roll()
+        {
+            return random.Next(1, 7);
+        }
+    }
+}

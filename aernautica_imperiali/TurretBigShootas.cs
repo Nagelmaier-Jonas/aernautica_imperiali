95d059cf9df30ce6d174adc59343e324b4911064
JonasNagi <j.nagelmaier@htlkrems.at>
2020-10-28 20:08:13 +0100

    3 new weapons by jony king

--- aernautica_imperiali/TurretBigShootas.cs
+++ aernautica_imperiali/TurretBigShootas.cs
@@ -1,5 +1,9 @@
 ﻿namespace aernautica_imperiali {
     public class TurretBigShootas : AWeapon{
-        
+        public TurretBigShootas(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage, int special) : base(new EFireArc[]
+            {
+                EFireArc.REAR,EFireArc.RIGHT,EFireArc.LEFT,EFireArc.UP
+            }, 3, 1, 0, 5, 0) {
+        }
     }
 }

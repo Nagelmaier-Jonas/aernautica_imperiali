3f8ffc6906e124f7e36e20a89cdc21c60f50aff4
Lukas <l.wagner@htlkrems.at>
2020-10-28 20:52:45 +0100

    Penis

--- aernautica_imperiali/QuadAutocannon.cs
+++ aernautica_imperiali/QuadAutocannon.cs
@@ -1,6 +1,6 @@
 ﻿namespace aernautica_imperiali {
     public class QuadAutocannon : AWeapon{
-        public QuadAutocannon(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage, int special) : base(new EFireArc[]{EFireArc.FRONT}, 2, 6, 0, 4, 0){
+        public QuadAutocannon(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage, int ammo, int special) : base(new EFireArc[]{EFireArc.FRONT}, 2, 6, 0, 4, -1, 0){
         }
     }
 }
--- aernautica_imperiali/QuadBigShootas.cs
+++ aernautica_imperiali/QuadBigShootas.cs
@@ -1,6 +1,6 @@
 ﻿namespace aernautica_imperiali {
     public class QuadBigShootas : AWeapon{
-        public QuadBigShootas(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage, int special) : base(new EFireArc[]{EFireArc.FRONT}, 8, 4, 0, 5, 0) {
+        public QuadBigShootas(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage, int ammo, int special) : base(new EFireArc[]{EFireArc.FRONT}, 8, 4, 0, 5, -1, 0) {
         }
     }
 }

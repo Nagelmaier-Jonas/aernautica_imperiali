65f77cef6739e449562aaaf3cfa05086f270cd3f
JonasNagi <j.nagelmaier@htlkrems.at>
2020-10-28 20:53:03 +0100

    niklas geht ma am oasch 2.0

--- aernautica_imperiali/TwinLasercannon.cs
+++ aernautica_imperiali/TwinLasercannon.cs
@@ -1,8 +1,8 @@
 ﻿namespace aernautica_imperiali {
     public class TwinLasercannon : AWeapon{
-        public TwinLasercannon(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage, int special) : base(new EFireArc[] {
+        public TwinLasercannon(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage,int ammo, int special) : base(new EFireArc[] {
             
-        }, 4, 6, 2, 5, 0) {
+        }, 4, 6, 2, 5,-1, 0) {
         }
     }
 }
--- aernautica_imperiali/TwinMultilasers.cs
+++ aernautica_imperiali/TwinMultilasers.cs
@@ -1,8 +1,8 @@
 ﻿namespace aernautica_imperiali {
     public class TwinMultilasers : AWeapon{
-        public TwinMultilasers(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage, int special) : base(new EFireArc[] {
+        public TwinMultilasers(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage,int ammo, int special) : base(new EFireArc[] {
             EFireArc.FRONT
-        }, 4, 6, 2, 5, 0) {
+        }, 4, 6, 2, 5,-1, 0) {
         }
     }
 }

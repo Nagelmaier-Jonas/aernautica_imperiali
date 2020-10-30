36ef7528349f0ff396309edabdcdb3dad7e28e67
JonasNagi <j.nagelmaier@htlkrems.at>
2020-10-30 19:23:59 +0100

    plane values

--- aernautica_imperiali/BigBurna.cs
+++ aernautica_imperiali/BigBurna.cs
@@ -1,6 +1,6 @@
 namespace aernautica_imperiali{
     public class BigBurna : APlane{
-        public BigBurna(Point p, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int maxAltitude, int planeValue, bool spin) : base(p, 3, speed, 2, 3, 7, 4, 4, 4, 22, spin){
+        public BigBurna(Point p, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int maxAltitude, int planeValue, bool spin) : base(p, 3, speed, 2, 3, 7, 4, 4, 4, 22, false){
         }
     }
 }
--- aernautica_imperiali/Executioner.cs
+++ aernautica_imperiali/Executioner.cs
@@ -1,6 +1,6 @@
 namespace aernautica_imperiali{
     public class Executioner : APlane{
-        public Executioner(int x, int y, int z, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int altitude, int maxAltitude, int planeValue, bool spin) : base(x, y, z, structure, speed, throttle, minSpeed, maxSpeed, maneuver, handling, altitude, maxAltitude, planeValue, spin){
+        public Executioner(Point p, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int maxAltitude, int planeValue, bool spin) : base(p, 3, speed, 2, 2, 7, 6, 3, 5, 23, false){
         }
     }
 }

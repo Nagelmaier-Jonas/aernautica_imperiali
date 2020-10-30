c4b132d3358965ac642948689d81a16c15ca89ea
Lukas <l.wagner@htlkrems.at>
2020-10-30 20:00:01 +0100

    Penis

--- aernautica_imperiali/Planes/Hellion.cs
+++ aernautica_imperiali/Planes/Hellion.cs
@@ -1,6 +1,6 @@
 namespace aernautica_imperiali{
     public class Hellion : APlane{
-        public Hellion(Point p, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int altitude, int maxAltitude, int planeValue, bool spin) : base(p, 2, speed, 3, 2, 8, 7, 2, 5, 26, false){
+        public Hellion(Point p, int speed) : base(p, 2, speed, 3, 2, 8, 7, 2, 5, 26, false, new AWeapon[]{new TwinMultilasers}){
         }
     }
 }
--- aernautica_imperiali/Planes/Vulture.cs
+++ aernautica_imperiali/Planes/Vulture.cs
@@ -1,6 +1,6 @@
 namespace aernautica_imperiali{
     public class Vulture : APlane{
-        public Vulture(Point p, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int altitude, int maxAltitude, int planeValue, bool spin) : base(p, 2, speed, 2, 3, 8, 5, 3, 4, 23, false){
+        public Vulture(Point p, int speed) : base(p, 2, speed, 2, 3, 8, 5, 3, 4, 23, false, new AWeapon[]{}){
         }
     }
 }

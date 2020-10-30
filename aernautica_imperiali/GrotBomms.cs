c49f735e7672928bffe7ade0b83a93cbf2274a8d
JonasNagi <j.nagelmaier@htlkrems.at>
2020-10-30 15:30:00 +0100

    remove GrotBomms

--- aernautica_imperiali/GrotBomms.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace aernautica_imperiali {
-    public class GrotBomms : AWeapon{
-        public GrotBomms(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage, int ammo, int special) : base(new EFireArc[]
-            {
-                EFireArc.FRONT
-            }, ?, ?, ?, 2, 2, 4) {
-        }
-    }
-}

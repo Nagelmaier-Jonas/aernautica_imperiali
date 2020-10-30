dedff036ef18067ed82c19732f489ff2615044b4
Lukas <l.wagner@htlkrems.at>
2020-10-30 22:20:35 +0100

    Penis

--- aernautica_imperiali/GameEngine.cs
+++ aernautica_imperiali/GameEngine.cs
@@ -23,20 +23,49 @@ namespace aernautica_imperiali {
         }
         
         public bool IsMoveLegal(APlane plane, Point destination) {
-            
+            foreach (Point p in plane.CalculateRoute(destination)){
+                if (!IsPointValid(p)) return false;
+            }
+
+            int speed = plane.Speed;
+            int maneuver = plane.Maneuver;
+
+            for (int i = 0; i < plane.CalculateRoute(destination).Count; i++){
+                if (plane.CalculateRoute(destination)[i].X != plane.CalculateRoute(destination)[i++].X &&
+                    plane.CalculateRoute(destination)[i].Y != plane.CalculateRoute(destination)[i++].Y){
+                    maneuver--;
+                    if (maneuver == 0) return false;
+                }
+                speed--;
+                if (speed == 0) return false;
+            }
+
+            return true;
+
         }
 
-        public void MovePlane(Point destination) {
-            
+        public void MovePlane(APlane plane, Point destination) {
+            if (IsMoveLegal(plane, destination)){
+                plane.Move(destination);
+                for (int i = 0; i < plane.CalculateRoute(destination).Count; i++){
+                    if (plane.CalculateRoute(destination)[i].X != plane.CalculateRoute(destination)[i++].X &&
+                        plane.CalculateRoute(destination)[i].Y != plane.CalculateRoute(destination)[i++].Y){
+                        plane.Maneuver--;
+                    }
+                    plane.Speed--;
+                }
+            }
         }
 
         public bool IsPointValid(Point p){
             if (!Map.GetInstance().IsPointLegal(p)) return false;
-            foreach (Player player in _ork){
-                foreach (APlane plane in player){
-                    if(plane.)
+                foreach (APlane plane in _ork.Planes){
+                    if (plane.X == p.X && plane.Y == p.Y && plane.Z == p.Z) return false;
                 }
-            }
+                foreach (APlane plane in _imperialis.Planes){
+                    if (plane.X == p.X && plane.Y == p.Y && plane.Z == p.Z) return false;
+                }
+                return true;
         }
 
         public bool CheckSpin() {

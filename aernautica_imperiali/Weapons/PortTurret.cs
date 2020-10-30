13a3423ffedf4de5baeb1ad851a3a68be10e31cf
JonasNagi <j.nagelmaier@htlkrems.at>
2020-10-30 20:02:26 +0100

    lukas fehlerkorrektur

--- aernautica_imperiali/Weapons/PortTurret.cs
+++ aernautica_imperiali/Weapons/PortTurret.cs
@@ -1,6 +1,6 @@
 ﻿namespace aernautica_imperiali {
     public class PortTurret : AWeapon{
-        public PortTurret(EFireArc[] fireArc, int shortpower, int mediumpower, int longpower, int damage,int ammo, int special) : base(new EFireArc[]{EFireArc.LEFT,EFireArc.UP}, 2, 1, 0, 5, -1, 0){
+        public PortTurret() : base(new EFireArc[]{EFireArc.LEFT,EFireArc.UP}, 2, 1, 0, 5, -1, 0){
         }
     }
 }

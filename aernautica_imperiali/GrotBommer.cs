a83959537ce4c1de7a9b322a8e3cc47fc79bc5b5
Lukas <l.wagner@htlkrems.at>
2020-10-30 19:28:30 +0100

    Penis

--- aernautica_imperiali/GrotBommer.cs
+++ aernautica_imperiali/GrotBommer.cs
@@ -1,6 +1,6 @@
 namespace aernautica_imperiali{
     public class GrotBommer : APlane{
-        public GrotBommer(int x, int y, int z, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int altitude, int maxAltitude, int planeValue, bool spin) : base(x, y, z, structure, speed, throttle, minSpeed, maxSpeed, maneuver, handling, altitude, maxAltitude, planeValue, spin){
+        public GrotBommer(Point p, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int altitude, int maxAltitude, int planeValue, bool spin) : base(p, 6, speed, 1, 2, 4, 3, 5, 4, 28, false){
         }
     }
 }
--- aernautica_imperiali/Vulture.cs
+++ aernautica_imperiali/Vulture.cs
@@ -1,6 +1,6 @@
 namespace aernautica_imperiali{
     public class Vulture : APlane{
-        public Vulture(int x, int y, int z, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int altitude, int maxAltitude, int planeValue, bool spin) : base(x, y, z, structure, speed, throttle, minSpeed, maxSpeed, maneuver, handling, altitude, maxAltitude, planeValue, spin){
+        public Vulture(Point p, int structure, int speed, int throttle, int minSpeed, int maxSpeed, int maneuver, int handling, int altitude, int maxAltitude, int planeValue, bool spin) : base(p, 2, speed, 2, 3, 8, 5, 3, 4, 23, false){
         }
     }
 }

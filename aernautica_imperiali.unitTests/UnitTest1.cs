f05cd7d8798067603f76dc550cf3fad1f6fac1da
Niklas <n.taube@htlkrems.at>
2020-11-04 12:17:24 +0100

    unittests

---
+++ aernautica_imperiali.unitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace aernautica_imperiali.unitTests {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}

aff72dadf5662e0565aff513eb9cc540490fc5f6
JonasNagi <j.nagelmaier@htlkrems.at>
2020-11-04 11:42:21 +0100

    bischl fehler dies das

--- aernautica_imperiali/Program.cs
+++ aernautica_imperiali/Program.cs
@@ -3,6 +3,9 @@
 namespace aernautica_imperiali {
     class Program {
         static void Main(string[] args) {
+            PlaneFactory factory = new PlaneFactory();
+            Point p = new Point(1,1,1);
+            GameEngine.GetInstance().Imperialis.Planes.Add(factory.Executioner(p,3));
             Map.GetInstance().PrintMap();
         }
     }

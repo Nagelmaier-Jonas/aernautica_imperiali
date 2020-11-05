01497909089c2a41fc5cf322a59408a8ec965246
JonasNagi <j.nagelmaier@htlkrems.at>
2020-11-05 08:33:22 +0100

    nix

--- aernautica_imperiali/GameEngine.cs
+++ aernautica_imperiali/GameEngine.cs
@@ -5,6 +5,7 @@ namespace aernautica_imperiali {
         private static GameEngine _instance = new GameEngine();
         private Player _imperialis = new Player();
         private Player _ork = new Player();
+        private bool _turnToken;
         private GameEngine() {
         }
 
@@ -16,6 +17,11 @@ namespace aernautica_imperiali {
         public Player Ork {
             get => _ork;
             set => _ork = value;
+        }
+
+        public bool TurnToken {
+            get => _turnToken;
+            set => _turnToken = value;
         }
 
         public static GameEngine GetInstance() {
@@ -40,18 +46,28 @@ namespace aernautica_imperiali {
         public void PlacePlane(Plane plane) {
             if (Map.GetInstance().IsPointLegal(plane)) {
                 if (plane.Faction == 'i') {
-                    if (plane.Y < 3 && _imperialis.StartPoints >= plane.PlaneValue) {
-                        _imperialis.StartPoints -= plane.PlaneValue;
-                        _imperialis.Planes.Add(plane);
+                    if (plane.Y < 3) {
+                        if (_imperialis.StartPoints >= plane.PlaneValue) {
+                            _imperialis.StartPoints -= plane.PlaneValue;
+                            _imperialis.Planes.Add(plane);
+                        }
+                        else {
+                            Logger.GetInstance().Info("You don't have enough Points: " + _imperialis.StartPoints + " left");
+                        }
                     }
                     else {
                         Logger.GetInstance().Info("Out of placement field");
                     }
                 }
                 else {
-                    if (plane.Y > 11 && _ork.StartPoints >= plane.PlaneValue) {
-                        _ork.StartPoints -= plane.PlaneValue;
-                        _ork.Planes.Add(plane);
+                    if (plane.Y > 11) {
+                        if (_ork.StartPoints >= plane.PlaneValue) {
+                            _ork.StartPoints -= plane.PlaneValue;
+                            _ork.Planes.Add(plane);
+                        }
+                        else {
+                            Logger.GetInstance().Info("You don't have enough Points: " + _ork.StartPoints + " left");
+                        }
                     }
                     else {
                         Logger.GetInstance().Info("Out of placement field");

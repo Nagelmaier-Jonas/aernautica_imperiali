ed3e0bdb0169736ee92124f7381940c37f775f76
Lukas <l.wagner@htlkrems.at>
2020-11-05 21:36:34 +0100

    Moin

--- aernautica_imperiali.unittest/GameEngineTest.cs
+++ aernautica_imperiali.unittest/GameEngineTest.cs
@@ -1,5 +1,24 @@
+using NUnit.Framework;
+
 namespace aernautica_imperiali.unittest{
     public class GameEngineTest{
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void SingeltonTest(){
+            
+        }
         
+        [Test]
+        public void CheckStructureTest(){
+            
+        }
+        
+        [Test]
+        public void PlacePlaneTest(){
+            
+        }
     }
 }

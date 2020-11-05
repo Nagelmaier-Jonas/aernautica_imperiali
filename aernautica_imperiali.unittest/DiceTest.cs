e3f0ae8ae16def7bfd646f5a0dd5884e3d0744ea
JonasNagi <j.nagelmaier@htlkrems.at>
2020-11-05 21:56:59 +0100

    dicetest

--- aernautica_imperiali.unittest/DiceTest.cs
+++ aernautica_imperiali.unittest/DiceTest.cs
@@ -3,7 +3,18 @@ namespace aernautica_imperiali.unittest {
     public class DiceTest {
 
         [Test]
-        public void Test() {
+        public void SingletonTest() {
+            Dice dice = Dice.GetInstance();
+            Dice dice2 = Dice.GetInstance();
+            
+            Assert.AreSame(dice,dice2);
+        }
+        
+        [Test]
+        public void Roll() {
+            int roll = Dice.GetInstance().Roll();
+            
+            Assert.IsTrue(roll >= 1 && roll <= 6);
             
         }
     }

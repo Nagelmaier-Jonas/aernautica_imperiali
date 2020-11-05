3f952d811f217240cd7905b4e63142cfe9f4e28b
JonasNagi <j.nagelmaier@htlkrems.at>
2020-11-05 09:03:13 +0100

    nix

--- aernautica_imperiali/DefaultMoveBehavior.cs
+++ aernautica_imperiali/DefaultMoveBehavior.cs
@@ -6,10 +6,7 @@ namespace aernautica_imperiali{
             if (plane.Faction == 'i') {
                 if (GameEngine.GetInstance().TurnToken) {
                     if (plane.IsMoveLegal(destination)) {
-                        plane.X += destination.X;
-                        plane.Y += destination.Y;
-                        plane.Z += destination.Z;
-                        GameEngine.GetInstance().TurnToken = !GameEngine.GetInstance().TurnToken;
+                        GameEngine.GetInstance().Move(plane,destination);
                     }
                 }
                 else {
@@ -20,10 +17,7 @@ namespace aernautica_imperiali{
             {
                 if (GameEngine.GetInstance().TurnToken == false) {
                     if (plane.IsMoveLegal(destination)) {
-                        plane.X += destination.X;
-                        plane.Y += destination.Y;
-                        plane.Z += destination.Z;
-                        GameEngine.GetInstance().TurnToken = !GameEngine.GetInstance().TurnToken;
+                        GameEngine.GetInstance().Move(plane,destination);
                     }
                 }
                 else {
@@ -33,59 +27,20 @@ namespace aernautica_imperiali{
         }
 
         public void Fire(Plane plane, Plane target, Weapon weapon) {
-            int dice;
             if (plane.Faction == 'i') {
-                if (!GameEngine.GetInstance().TurnToken) {
+                if (GameEngine.GetInstance().TurnToken) {
+                    GameEngine.GetInstance().Fire(plane,target,weapon);
+                }
+                else {
                     Logger.GetInstance().Info("It's not your turn");
                 }
             }
-            else if (GameEngine.GetInstance().TurnToken) {
-                Logger.GetInstance().Info("It's not your turn");                
-            }
-            if (plane.CanFire(target,weapon)) {
-                ERange range = plane.CheckRange(target);
-                switch (range) {
-                    case ERange.SHORT:
-                        for (int i = 0; i < weapon.Firepower[ERange.SHORT]; i++) {
-                            dice = Dice.GetInstance().Roll();
-                            if (dice >= weapon.Damage) {
-                                target.Structure--;
-                            }
-
-                            if (dice >= weapon.Special) {
-                                target.Structure--;
-                            }
-                        }
-                        break;
-                    case ERange.MEDIUM:
-                        for (int i = 0; i < weapon.Firepower[ERange.MEDIUM]; i++) {
-                            dice = Dice.GetInstance().Roll();
-                            if (dice >= weapon.Damage) {
-                                target.Structure--;
-                            }
-
-                            if (dice >= weapon.Special) {
-                                target.Structure--;
-                            }
-                        }
-                        break;
-                    case ERange.LONG:
-                        for (int i = 0; i < weapon.Firepower[ERange.LONG]; i++) {
-                            dice = Dice.GetInstance().Roll();
-                            if (dice >= weapon.Damage) {
-                                target.Structure--;
-                            }
-
-                            if (dice >= weapon.Special) {
-                                target.Structure--;
-                            }
-                        }
-                        break;
-                    case ERange.OUTOFRANGE:
-                        Logger.GetInstance().Info("Target is out of Range");
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
+            else {
+                if (!GameEngine.GetInstance().TurnToken) {
+                    GameEngine.GetInstance().Fire(plane,target,weapon);     
+                }
+                else {
+                    Logger.GetInstance().Info("It's not your turn");      
                 }
             }
         }
--- aernautica_imperiali/GameEngine.cs
+++ aernautica_imperiali/GameEngine.cs
@@ -91,6 +91,63 @@ namespace aernautica_imperiali {
                 Logger.GetInstance().Info("Orks won");
             }
         }
+
+        public void Fire(Plane plane, Plane target, Weapon weapon) {
+            int dice;
+             if (plane.CanFire(target,weapon)) {
+                ERange range = plane.CheckRange(target);
+                switch (range) {
+                    case ERange.SHORT:
+                        for (int i = 0; i < weapon.Firepower[ERange.SHORT]; i++) {
+                            dice = Dice.GetInstance().Roll();
+                            if (dice >= weapon.Damage) {
+                                target.Structure--;
+                            }
+
+                            if (dice >= weapon.Special) {
+                                target.Structure--;
+                            }
+                        }
+                        break;
+                    case ERange.MEDIUM:
+                        for (int i = 0; i < weapon.Firepower[ERange.MEDIUM]; i++) {
+                            dice = Dice.GetInstance().Roll();
+                            if (dice >= weapon.Damage) {
+                                target.Structure--;
+                            }
+
+                            if (dice >= weapon.Special) {
+                                target.Structure--;
+                            }
+                        }
+                        break;
+                    case ERange.LONG:
+                        for (int i = 0; i < weapon.Firepower[ERange.LONG]; i++) {
+                            dice = Dice.GetInstance().Roll();
+                            if (dice >= weapon.Damage) {
+                                target.Structure--;
+                            }
+
+                            if (dice >= weapon.Special) {
+                                target.Structure--;
+                            }
+                        }
+                        break;
+                    case ERange.OUTOFRANGE:
+                        Logger.GetInstance().Info("Target is out of Range");
+                        break;
+                }
+
+                _turnToken = !_turnToken;
+             }
+        }
+
+        public void Move(Plane plane, Point destination) {
+            plane.X += destination.X;
+            plane.Y += destination.Y;
+            plane.Z += destination.Z;
+            _turnToken = !_turnToken;
+        }
         
     }
 }

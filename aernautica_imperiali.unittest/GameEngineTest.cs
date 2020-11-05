6fd33501bd1ac0a216c5429024e69089f743b009
Lukas <l.wagner@htlkrems.at>
2020-11-05 22:41:24 +0100

    Moin

--- aernautica_imperiali.unittest/GameEngineTest.cs
+++ aernautica_imperiali.unittest/GameEngineTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using aernautica_imperiali;
 
 namespace aernautica_imperiali.unittest {
     public class GameEngineTest {
@@ -6,9 +7,49 @@ namespace aernautica_imperiali.unittest {
         [SetUp]
         public void Setup() {
         }
+
+        [Test]
+        public void TestSingleton(){
+            GameEngine gameEngine = GameEngine.GetInstance();
+            GameEngine gameEngine2 = GameEngine.GetInstance();
+            Assert.AreSame(gameEngine,gameEngine2);
+        }
         
         [Test]
-        public void Test() {
+        public void TestCheckStructure() {
+            PlaneFactory factory = new PlaneFactory();
+            GameEngine.GetInstance().PlacePlane(factory.Executioner(new Point(3,3,3), 3));
+            GameEngine.GetInstance().Imperialis.Planes[0].Structure = 0;
+            Assert.AreEqual(0,GameEngine.GetInstance().Imperialis.Planes.Count);
+        }
+        
+        [Test]
+        public void TestPlacePlane(){
+            
+        }
+        
+        [Test]
+        public void Test(){
+            
+        }
+        
+        [Test]
+        public void Test(){
+            
+        }
+        
+        [Test]
+        public void Test(){
+            
+        }
+        
+        [Test]
+        public void Test(){
+            
+        }
+        
+        [Test]
+        public void Test(){
             
         }
     }

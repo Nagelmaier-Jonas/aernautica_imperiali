ec918686290249488f092b312a7cd4f3533f047d
Lukas <l.wagner@htlkrems.at>
2020-11-06 09:29:21 +0100

    Moin

--- aernautica_imperiali/Program.cs
+++ aernautica_imperiali/Program.cs
@@ -9,6 +9,39 @@ namespace aernautica_imperiali {
             Plane e = factory.Executioner(p, 1);
             e.CheckSpin();
             Console.WriteLine(e.MoveBehavior);
+            
+            GameEngine.GetInstance().PlacePlane(factory.Executioner(new Point(0,1,2), 4));
+            GameEngine.GetInstance().PlacePlane(factory.Executioner(new Point(2,2,4), 4));
+            GameEngine.GetInstance().PlacePlane(factory.Hellion(new Point(4,1,3), 4));
+            GameEngine.GetInstance().PlacePlane(factory.BlueDevil(new Point(6,2,2), 4));
+            GameEngine.GetInstance().PlacePlane(factory.Executioner(new Point(9,1,3), 4));
+            GameEngine.GetInstance().PlacePlane(factory.BlueDevil(new Point(13,2,1), 4));
+            
+            GameEngine.GetInstance().PlacePlane(factory.BigBurna(new Point(2,12,2), 4));
+            GameEngine.GetInstance().PlacePlane(factory.GrotBommer(new Point(4,13,4), 4));
+            GameEngine.GetInstance().PlacePlane(factory.GrotBommer(new Point(6,12,3), 4));
+            GameEngine.GetInstance().PlacePlane(factory.Vulture(new Point(8,14,2), 4));
+            GameEngine.GetInstance().PlacePlane(factory.Vulture(new Point(11,13,3), 4));
+            GameEngine.GetInstance().PlacePlane(factory.BigBurna(new Point(13,12,3), 4));
+            
+            GameEngine.GetInstance().Imperialis.Planes[0].Move(new Point(1,4,2));
+            GameEngine.GetInstance().Ork.Planes[0].Move(new Point(3,9,3));
+            
+            GameEngine.GetInstance().Imperialis.Planes[1].Move(new Point(2,5,3));
+            GameEngine.GetInstance().Ork.Planes[1].Move(new Point(4,10,4));
+            
+            GameEngine.GetInstance().Imperialis.Planes[2].Move(new Point(5,2,5));
+            GameEngine.GetInstance().Ork.Planes[2].Move(new Point(5,10,4));
+            
+            GameEngine.GetInstance().Imperialis.Planes[3].Move(new Point(6,2,2));
+            GameEngine.GetInstance().Ork.Planes[3].Move(new Point(8,14,2));
+            
+            GameEngine.GetInstance().Imperialis.Planes[4].Move(new Point(9,1,3));
+            GameEngine.GetInstance().Ork.Planes[4].Move(new Point(11,13,3));
+            
+            GameEngine.GetInstance().Imperialis.Planes[5].Move(new Point(13,2,1));
+            GameEngine.GetInstance().Ork.Planes[5].Move(new Point(13,12,3));
+
         }
     }
 }

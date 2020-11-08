f73708bcfc025e975cd68452766cf90e068c2315
JonasNagi <j.nagelmaier@htlkrems.at>
2020-11-08 21:28:25 +0100

    SOMETHING

--- aernautica_imperiali/Program.cs
+++ aernautica_imperiali/Program.cs
@@ -7,6 +7,7 @@ namespace aernautica_imperiali {
     class Program {
         static void Main(string[] args) {
             Logger.LOG_TO_CONSOLE = true;
+            
             //Place
             GameEngine.GetInstance().PlacePlane(PlaneFactory.Executioner(new Point(2, 1, 3), 3));
             GameEngine.GetInstance().PlacePlane(PlaneFactory.GrotBommer(new Point(2, 13, 3), 3));
